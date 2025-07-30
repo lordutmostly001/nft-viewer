41c932b94c15e5ca6428b630bfcee929aeadf14a
GoPie <myselfgopie@gmail.com>
2025-07-30 11:23:47 +0530

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '^three$': '<rootDir>/node_modules/three/build/three.cjs',
+  },
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/packages/fiber/dist',
+    '<rootDir>/packages/fiber/src/index',
+    '<rootDir>/packages/test-renderer/dist',
+  ],
+  coverageDirectory: './coverage/',
+  collectCoverage: false,
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  verbose: false,
+  testTimeout: 30000,
+  setupFilesAfterEnv: ['<rootDir>/packages/shared/setupTests.ts'],
+}
